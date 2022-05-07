e3de1f897e30e7270ff98bcfdc49f3bb6eed4176
Jenna Allgeier <jenna.allgeier@gmail.com>
2022-05-07 17:55:25 -0400

    added user foreign key to event model

--- server/giverly/models.py
+++ server/giverly/models.py
@@ -13,6 +13,7 @@ class User(models.Model):
         return self.email
 
 class Event(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
     title = models.CharField(max_length=200)
     description = models.TextField()
     start_time = models.DateTimeField()
