fd5f4c3a1be04e4988f69e370459d619252f1450
Jenna Allgeier <jenna.allgeier@gmail.com>
2022-05-07 19:40:48 -0400

    need to fix plural order itemss

--- server/giverly/admin.py
+++ server/giverly/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Event, Order, OrderItems
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Event)
+admin.site.register(Order)
+admin.site.register(OrderItems)
