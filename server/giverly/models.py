2cb9c1df0251c0edd6abfef339178636b2f758e3
Jenna Allgeier <jenna.allgeier@gmail.com>
2022-05-08 09:40:43 -0400

    added image fields to order and orderItem and quantity to orderItem

--- server/giverly/models.py
+++ server/giverly/models.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from django.db import models
 
 # Create your models here.
@@ -16,6 +17,7 @@ class Event(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
     title = models.CharField(max_length=200)
     description = models.TextField(null=True)
+    image = models.TextField(null=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
@@ -35,7 +37,9 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='orderItems')
     itemName = models.CharField(max_length=100, null=True)
     itemDetails = models.CharField(max_length=500, null=True)
+    image = models.TextField(null=True)
     price = models.DecimalField(max_digits = 7, decimal_places = 2, null=True)
+    quantity = models.PositiveSmallIntegerField(null=True)
 
     def __str__(self):
         return self.itemName
