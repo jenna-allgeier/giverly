b75539045192dcdc393e0ae03292073e351a9be0
Jenna Allgeier <jenna.allgeier@gmail.com>
2022-05-10 08:13:19 -0400

    user and event views, serializers and urls working

--- server/giverly/views.py
+++ server/giverly/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import generics
 from .models import User, Event, Order, OrderItem
+from .serializers import UserSerializer, EventSerializer
 
-# Create your views here.
+# Initial test view.
 
-def user_detail(request, pk):
-    user = User.objects.get(id=pk)
-    return render(request, 'giverly/user_detail.html', {'user': user})
+# def user_detail(request, pk):
+#     user = User.objects.get(id=pk)
+#     return render(request, 'giverly/user_detail.html', {'user': user})
 
 # make user profile with user details and event
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class EventList(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
