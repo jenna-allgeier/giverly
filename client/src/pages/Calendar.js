554c11ba28dd51e755d7e248b2b36ea5adce8a08
Jenna Allgeier <jenna.allgeier@gmail.com>
2022-05-12 18:20:07 -0400

    styled calendar and pages

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,12 +1,24 @@
+import { useState } from 'react'
 import { connect } from "react-redux"
 import EventDetails from "../components/EventDetails"
+import Calendar from 'react-calendar'
 
 
-const Calendar = (props) => {
+
+const AppCalendar = (props) => {
+
+    const [value, onChange] = useState(new Date());
 
     return (
-        <div>
-            <EventDetails />
+        <div className='calendar-container'>
+            
+            <div>
+                <Calendar className={'calendar'} onChange={onChange} value={value} />
+            </div>
+            <h2>Add Your Events</h2>
+            <div className='events-container'>
+                <EventDetails />
+            </div>
         </div>
     )
 }
@@ -19,4 +31,4 @@ const mapActionsToProps = (dispatch) => {
     return {}
 }
 
-export default connect(mapStateToProps, mapActionsToProps)(Calendar)
+export default connect(mapStateToProps, mapActionsToProps)(AppCalendar)
